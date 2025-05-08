bab7dca3f995d65f43ef0ba33cb0853dc5e30844
NewJKH <newsun0805@gmail.com>
2025-05-08 14:27:14 +0900

    add util:날짜 포멧

---
+++ src/main/java/org/jkh/planit/util/DateTimeUtil.java
@@ -0,0 +1,13 @@
+package org.jkh.planit.util;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class DateTimeUtil {
+    public static Timestamp toTimestamp(String date){
+        LocalDate localDate = LocalDate.parse(date);
+        LocalDateTime localDateTime = localDate.atStartOfDay();
+        return Timestamp.valueOf(localDateTime);
+    }
+}

a6d5d66d78f176d2ed265bc5d16d460872a28f7d
NewJKH <newsun0805@gmail.com>
2025-05-09 10:33:59 +0900

    update : 커스텀 예외 수정

--- src/main/java/org/jkh/planit/exception/PlanNotFoundException.java
+++ src/main/java/org/jkh/planit/exception/PlanNotFoundException.java
@@ -4,4 +4,7 @@ public class PlanNotFoundException extends RuntimeException {
     public PlanNotFoundException(String message) {
         super(message);
     }
+   public PlanNotFoundException() {
+     super(" 수정할 대상이 없습니다. ");
+   }
 }
